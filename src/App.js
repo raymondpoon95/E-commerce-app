b8dbe12d48911b5b9dbda421323f6a736b13d4d6
Raymond Poon <raymondpoon@Evas-MacBook-Air.local>
2022-02-09 22:34:09 +0000

    updated with shop page, collection-item and preview-collection component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import ShopPage from "./pages/shop/Shop";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
